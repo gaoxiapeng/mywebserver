44fc8709985d44cc531e700562fb7cab33009f40
gaoxiapeng <1208711879@qq.com>
2025-10-10 21:55:27 +0800

    请求报文

---
+++ code/http/httprequest.cpp
@@ -0,0 +1,263 @@
+#include "httprequest.h"
+
+// 默认HTTP页面集合，用于路径补全
+const std::unordered_set<std::string> HttpRequest::DEFAULT_HTML {
+    "/index", "/welcome", "/video", "/picture",
+    "/register", "login",
+};
+// /register和/login需要区分注册和登记的业务逻辑，其余只需要补全路径
+const std::unordered_map<std::string, int> HttpRequest::DEFAULT_HTML_TAG {
+    {"/register", 0}, {"/login", 1},
+};
+
+void HttpRequest::Init() {
+    method_ = path_ = version_ = body_ = "";
+    state_ = REQUEST_LINE;
+    header_.clear();
+    post_.clear();
+}
+
+bool HttpRequest::IsKeepAlive() const {
+    if(header_.count("Connection") == 1) {
+        // 1、要求显示表达HTTP版本为1.1  2、Connection模式必须是keep-alive
+        return header_.find("Connection")->second == "keep-alive" && version_ == "1.1";
+    }
+    return false;
+}
+
+/* 一个POST方法的请求报文
+POST /api/user HTTP/1.1         // 请求行
+Host: example.com               // 请求头
+Content-Type: application/json
+Content-Length: 123
+
+{                               // 请求体
+   "name": "John Doe",
+   "email": "johndoe@example.com",
+   "age": 30
+}
+*/
+bool HttpRequest::parse(Buffer& buff) {
+    // 请求报文中，每一行以CRLF-回车换行符为结尾
+    const char CRLF[] = "\r\n";
+    if(buff.ReadableBytes() <= 0) {
+        return false;
+    }
+    // 逐行解析报文
+    while(buff.ReadableBytes() && state_ != FINISH) {
+        // 提取一行报文
+        /*search()：在[buff.Peak, buff.BeginWriteConst)中查找子序列[CRLF, CRLF+2)的第一次出现位置*/
+        /*C风格中，char数组末尾以\0为结尾，因此CRLF在buff中表示为\r\n\0，因此lineEnd='\r'*/
+        const char* lineEnd = std::search(buff.Peek(), buff.BeginWriteConst(), CRLF, CRLF + 2);
+        std::string line(buff.Peek(), lineEnd);
+        switch(state_)
+        {
+        /*
+            有限状态机，从请求行开始，每处理完后会自动转入到下一个状态    
+        */
+        case REQUEST_LINE:
+            if(!ParseRequestLine_(line)) {   // 解析失败
+                return false;
+            }
+            // 若解析成功，ParseRequsetLine_()将状态转换为HEADERS
+            ParsePath_();
+            break;
+        case HEADERS:
+            ParseHeader_(line);
+            // 只有CRLF或无数据(代表ParseHeader_()部分也不会跳转到BODY部分)
+            if(buff.ReadableBytes() <= 2) {
+                state_ = FINISH;
+            }
+            break;
+        case BODY:
+            ParseBody_(line);
+            break;
+        default:
+            break;
+        }
+        // 未找到CRLF，跳出循环，等待下次数据到来
+        if(lineEnd == buff.BeginWrite()) {
+            break;
+        }
+        buff.RetrieveUntil(lineEnd + 2);
+    }
+    // LOG是C风格函数，无法识别string，只能用c_str()将其转换为const char*
+    LOG_ERROR("[%s], [%s], [%s]", method_.c_str(), path_.c_str(), version_.c_str());
+    return true;
+}
+
+/*
+http://www.bilibili.com/，识别到根路径"/"，最终路径变为/index.html
+http://www.bilibili.com/index，识别到预定义路径"/index"，最终路径为/index.html
+*/
+void HttpRequest::ParsePath_() {
+    // 预处理根路径
+    if(path_ == "/") {
+        path_ = "/index.html";
+    }
+    // 处理预定义路径
+    else {
+        for(auto &item : DEFAULT_HTML) {
+            if(item == path_) {
+                path_ += ".html";
+                break;
+            }
+        }
+    }
+}
+
+bool HttpRequest::ParseRequestLine_(const std::string& line) {
+    // 编译正则表达式模式
+    std::regex patten("^([^ ]*) ([^ ]*) HTTP/([^ ]*)$");
+    // 存储正则表达式匹配结果
+    std::smatch subMatch;
+    if(std::regex_match(line, subMatch, patten)) {
+        method_ = subMatch[1];
+        path_ = subMatch[2];
+        version_ = subMatch[3];
+        state_ = HEADERS;
+        return true;
+    }
+    else {
+        LOG_ERROR("RequestLine Error");
+        return false;
+    }
+}
+
+void HttpRequest::ParseHeader_(const std::string& line) {
+    std::regex patten("^([^:]*): ?(.*)$");
+    std::smatch subMatch;
+    if(std::regex_match(line, subMatch, patten)) {
+        header_[subMatch[1]] = subMatch[2];
+    }
+    // 结合parse来看，
+    else {
+        state_ = BODY;     // 遇到空行，代表HEADER部分结束，跳转到BODY
+    }
+}
+
+void HttpRequest::ParseBody_(const std::string& line) {
+    body_ = line;
+    ParsePost_();   // 解析post参数
+    state_ = FINISH;
+    LOG_DEBUG("Body:%s, len:%s", line.c_str(), line.size());
+}
+
+// 16进制转10进制
+int HttpRequest::ConverHex(char ch) {
+    if(ch >= 'A' && ch <= 'F') {
+        return ch - 'A' + 10;
+    }
+    if(ch >= 'a' && ch <= 'f') {
+        return ch - 'a' + 10;
+    }
+    return ch;
+}
+
+// 处理POST请求——解析表单数据(处理后的，post_中的)并进行用户验证
+void HttpRequest::ParsePost_() {
+    if(method_ == "POST" && header_["Connect-Type"] == "application/x-www-form-urlencoded") {
+        // 将POST表单数据映射到post_这一map里
+        ParseFromUrlencoded_();  
+        if(DEFAULT_HTML_TAG.count(path_)) {
+            int tag = DEFAULT_HTML_TAG.find(path_)->second;
+            LOG_DEBUG("Tag:%d", tag);
+            if(tag == 0 || tag == 1) {
+                bool isLogin = (tag == 1);
+                if(UserVerify(post_["username"], post_["password"], isLogin)) {
+                    path_ = "/welcome.html";
+                }
+                else {
+                    path_ = "/error.html";
+                }
+            }
+        }
+    }
+}
+
+// 解析的是POST请求体中的数据(且这些数据用的是URL编码格式)，存放到post_里
+void HttpRequest::ParseFromUrlencoded_() {
+    if(body_.size() == 0) {
+        return;
+    }
+
+    std::string key, value;
+    int num = 0;
+    int n = body_.size();
+    int i = 0, j = 0;
+
+    for(; i < n; i++) {
+        char ch = body_[i];
+        switch(ch)
+        {
+        // '='的前面为key，后面为value
+        case '=':
+            key = body_.substr(j, i-j); // substr(pos, len)
+            j = i + 1;
+            break;
+        // '&'代表一个键值对的结束
+        case '&':
+            value = body_.substr(j, i-j);
+            j = i + 1;
+            post_[key] = value;
+            LOG_DEBUG("key:%s, value:%s", key.c_str(), value.c_str());
+            break;
+        // '+'解码为空格
+        case '+':
+            body_[i] = ' ';
+            break;
+        // '%HH'
+        case '%':
+            num = ConverHex(body_[i+1]) * 16 + ConverHex(body_[i+2]);
+            // 先由16进制数转为10进制，再转为ASCII码重新存入
+            // 这地方似乎有问题？不应该是将num转为ASCII码后存入吗？为什么要分开个位和十位呢？
+            body_[i+1] = num / 10 + '0';
+            body_[i+2] = num % 10 + '0';
+            break;
+        default:
+            break;
+        }
+    }
+    // 此时i=n-1，j=最后一个'&'后一个
+    assert(j <= i);
+    if(post_.count(key) == 0 && j < i) {
+        value = body_.substr(j, i-j);
+        post_[key] = value;
+    }
+}
+
+bool HttpRequest::UserVerify(const std::string &name, const std::string &pwd, bool isLogin) {
+
+}
+
+
+std::string HttpRequest::path() const{
+    return path_;
+}
+
+std::string& HttpRequest::path(){
+    return path_;
+}
+std::string HttpRequest::method() const {
+    return method_;
+}
+
+std::string HttpRequest::version() const {
+    return version_;
+}
+
+std::string HttpRequest::GetPost(const std::string& key) const {
+    assert(key != "");
+    if(post_.count(key) == 1) {
+        return post_.find(key)->second;
+    }
+    return "";
+}
+
+std::string HttpRequest::GetPost(const char* key) const {
+    assert(key != nullptr);
+    if(post_.count(key) == 1) {
+        return post_.find(key)->second;
+    }
+    return "";
+}
---
+++ code/http/httprequest.h
@@ -0,0 +1,88 @@
+#ifndef HTTP_REQUEST_H
+#define HTTP_REQUSET_H
+
+#include <string>
+#include <regex>  // 正则表达式
+#include <errno.h>  // 错误号定义
+#include <mysql/mysql.h>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "../buffer/buffer.h"
+#include "../log/log.h"
+#include "../pool/sqlconnpool.h"
+
+/*
+​1、​初始化​​：创建对象时调用 Init()初始化所有成员变量。
+​​2、解析请求​​：调用 parse(Buffer& buff)开始解析，按照状态逐步解析：
+    - 首先解析请求行（ParseRequestLine_）
+    - 然后解析请求头（ParseHeader_）
+    - 最后解析请求体（ParseBody_）
+​​3、数据处理​​：解析过程中填充成员变量，如 method_、path_、header_等。
+​​4、结果获取​​：通过公共成员函数获取解析结果。
+*/
+
+class HttpRequest {
+public: 
+//枚举，定义了HTTP请求解析的不同阶段
+    enum PARSE_STATE {
+        REQUEST_LINE,
+        HEADERS,
+        BODY,
+        FINISH,
+    };
+
+// 表示服务器在处理HTTP请求时的不同状态和结果，并非标准状态码
+    enum HTTP_CODE {
+        NO_REQUEST = 0,
+        GET_REQUSET,
+        BAD_REQUSET,
+        NO_RESOURSE,
+        FORBIDDEND_REQUEST,
+        FILE_REQUEST,
+        INTERNAL_ERROR,
+        CLOSED_CONNECTION,
+    };
+
+    HttpRequest() {Init();}
+    ~HttpRequest() = default;
+
+    void Init();
+    bool parse(Buffer& buff);       // 从缓冲区解析HTTP请求
+
+    std::string path() const;       // 获取请求路径
+    std::string& path();
+    std::string method() const;     // 获取请求方法
+    std::string version() const;    // 获取HTTP版本
+    std::string GetPost(const std::string& key) const;  // 获取POST参数
+    std::string GetPost(const char* key) const;               // C风格
+    bool IsKeepAlive() const;       // 检查是否是持久连接
+
+
+private:
+    // 解析相关函数
+    bool ParseRequestLine_(const std::string& line);
+    void ParseHeader_(const std::string& line);
+    void ParseBody_(const std::string& line);
+
+    void ParsePath_();    // 处理请求路径
+    void ParsePost_();    // 处理POST请求
+    void ParseFromUrlencoded_();    // 解析URL编码的表单数据
+
+    // 用户验证函数
+    static bool UserVerify(const std::string& name, const std::string& pwd, bool isLogin);
+    static int ConverHex(char ch);      // 16进制字符转换为10进制
+
+    PARSE_STATE state_;     // 当前解析状态
+    std::string method_, path_, version_, body_;
+    std::unordered_map<std::string,std::string> header_;    // 请求头键值对：Content-Type和Content-Length
+    std::unordered_map<std::string,std::string> post_;      // POST参数键值对
+
+    static const std::unordered_map<std::string,int> DEFAULT_HTML_TAG;  // HTML标签映射
+    static const std::unordered_set<std::string> DEFAULT_HTML;  // 默认HTML页面集合
+};
+
+
+
+
+#endif
